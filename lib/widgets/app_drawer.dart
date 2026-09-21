57c67914fb14e0fb8202e51bf509c374c3d2a901
ouimwotu9w <yassinnhih@gmail.com>
2026-09-21 16:24:05 +0300

    fix: remove unused provider import

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
 import 'package:share_plus/share_plus.dart';
 
 import 'package:al_urjuza/data/poem_data.dart';
