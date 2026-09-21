dbf01e3ee11f98250bddb4118e7d29c84e1983b2
ouimwotu9w <yassinnhih@gmail.com>
2026-09-21 06:04:21 +0300

    test: replace stale generated Flutter smoke test

--- al_urjuza/test/widget_test.dart
+++ al_urjuza/test/widget_test.dart
@@ -1,30 +1,29 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:al_urjuza/main.dart';
+import 'package:al_urjuza/app.dart';
+import 'package:al_urjuza/providers/library_provider.dart';
+import 'package:al_urjuza/providers/progress_provider.dart';
+import 'package:al_urjuza/providers/settings_provider.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Al-Urjuza app smoke test', (WidgetTester tester) async {
+    SharedPreferences.setMockInitialValues({});
+    final prefs = await SharedPreferences.getInstance();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => SettingsProvider(prefs)),
+          ChangeNotifierProvider(create: (_) => LibraryProvider(prefs)),
+          ChangeNotifierProvider(create: (_) => ProgressProvider(prefs)),
+        ],
+        child: const AlUrjuzaApp(),
+      ),
+    );
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byType(AlUrjuzaApp), findsOneWidget);
   });
 }
